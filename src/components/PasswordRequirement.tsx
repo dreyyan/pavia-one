2ff53e2a656f8c3eafc357b0dbbe0e84095aaebc
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-01 21:07:00 +0800

    style: change indicator styling

--- src/components/PasswordRequirement.tsx
+++ src/components/PasswordRequirement.tsx
@@ -7,12 +7,11 @@ const PasswordRequirement = (props: PasswordRequirementProps) => {
     return (
         <div className="flex items-center gap-x-2">
             {/* Check Indicator */}
-            <div className={`rounded-full border size-4 ${props.isMet ? "bg-green-500 border-green-500" : "border-gray-300"}`}>
-
+            <div className={`text-xs text-center text-[var(--color-text-50)] rounded-full border size-4 ${props.isMet ? "bg-[var(--color-primary-600)] border-[var(--color-primary-600)]" : "border-gray-300"}`}>
             </div>
 
             {/* Requirement */}
-            <p className="label-caption text-[var(--color-text-400)]">{props.requirement}</p>
+            <p className="label-caption text-[var(--color-text-600)]">{props.requirement}</p>
         </div>
     );
 };
