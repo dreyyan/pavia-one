78c4d1b1970f3c8b21ee6fbdd84eaf8e4a76644e
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-03 00:50:27 +0800

    fix: split default login routes for admin and adviser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+
+// [IMPORT] Components
 import Layout from "./Layout";
-import Login from "./pages/Login";
+import AdminLogin from "./pages/AdminLogin"; 
+import AdviserLogin from "./pages/AdviserLogin";
 import ForgotPassword from "./pages/ForgotPassword";
 import AboutUs from "./AboutUs";
 
@@ -10,7 +13,8 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login/admin" element={<AdminLogin />} />
+        <Route path="/login/adviser" element={<AdviserLogin />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/about-us" element={<AboutUs />} />
       </Route>
