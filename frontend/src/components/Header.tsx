4cfd63505891f95dced2d79d3805dc2621ef3a1b
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-13 16:14:26 +0800

    style(header): update background color and banner

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,14 +6,14 @@ const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
-        <header className="flex justify-between items-center px-6 py-4">
+        <header className="flex justify-between items-center px-6 py-4 bg-[var(--color-primary-700)]">
             { isLoggedIn && 
             <button className="size-8 cursor-pointer">
                 <img src="/burger-menu-icon.svg" alt="Burger Menu Icon"/>
             </button>
             }
             <button onClick={() => navigate("/")} className="cursor-pointer">
-                <img src="/pavia-one-banner.svg" className="h-7" />
+                <img src="/pavia-one-banner-white.svg" className="h-7" />
             </button>
         </header>
     );
