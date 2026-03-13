81e8bd2e27dc79ea3beaee0ea279f76c26e59c3f
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-13 15:00:15 +0800

    add(routes): admin and adviser dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import Home from "./pages/Home";
 
 // [IMPORT] Components
 import Layout from "./Layout";
-import AdminLogin from "./pages/AdminLogin"; 
-import AdviserLogin from "./pages/AdviserLogin";
+import AdminLogin from "./pages/admin/AdminLogin"; 
+import AdviserLogin from "./pages/adviser/AdviserLogin";
 import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import AboutUs from "./AboutUs";
+import AdviserDashboard from "./pages/adviser/AdviserDashboard";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login/adviser" element={<AdviserLogin />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route path="/adviser/dashboard" element={<AdviserDashboard />} />
         <Route path="/about-us" element={<AboutUs />} />
       </Route>
     </Routes>
