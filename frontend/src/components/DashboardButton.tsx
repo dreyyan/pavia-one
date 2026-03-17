22c85108efc98dd635b35b9ded67ba540567ec15
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-17 18:16:51 +0800

    modify(dashboard-button): add 'to' field for link navigation

--- frontend/src/components/DashboardButton.tsx
+++ frontend/src/components/DashboardButton.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 interface DashboardButtonProps {
     iconSrc?: string;
     text: string;
     color: string;
+    to?: string;
 };
 
-const DashboardButton: React.FC<DashboardButtonProps> = ({iconSrc, text, color}) => {
+const DashboardButton: React.FC<DashboardButtonProps> = ({ iconSrc, text, color, to }) => {
+    const navigate = useNavigate();
+
+    // [HANDLE] Navigation
+    const handleClick = () => {
+        if (to) {
+        navigate(to); // Navigate if 'to' prop is provided
+        }
+    };
+
     return (
         <button
         style={{backgroundColor: color}}
+        onClick={handleClick}
         className="flex flex-col justify-center items-center aspect-square rounded-lg cursor-pointer transition-all duration-200 hover:opacity-80">
             {/* Icon */}
             <img src={iconSrc} className="size-16"/>
