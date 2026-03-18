eebd2d7123f91ecd0d638229c1ced1715fb54a88
Sean Benedict S. Besana <seanbenedict.besana@wvsu.edu.ph>
2026-03-18 22:59:01 +0800

    add: import and route for AdviserProfile.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,8 @@ import AboutUs from "./AboutUs";
 import AdviserDashboard from "./pages/adviser/AdviserDashboard";
 import AdviserClassManagement from "./pages/adviser/AdviserClassManagement";
 import AdviserMyClass from "./pages/adviser/AdviserMyClass";
+import AdviserProfile from "./pages/adviser/AdviserProfile";
+
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
         <Route path="/adviser/dashboard" element={<AdviserDashboard />} />
         <Route path="/adviser/classes" element={<AdviserClassManagement />} />
         <Route path="/adviser/classes/:id" element={<AdviserMyClass />} />
+        <Route path="/adviser/profile" element={<AdviserProfile />} />
 
         {/* Shared Endpoints */}
         <Route path="/about-us" element={<AboutUs />} />
