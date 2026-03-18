353c239907f78784349aa0b0b5d3e3427bd43175
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-18 18:02:09 +0800

    feat(adviser/dashboard): add 'to' link in dashboard button for 'View Students'

--- frontend/src/pages/adviser/AdviserDashboard.tsx
+++ frontend/src/pages/adviser/AdviserDashboard.tsx
@@ -112,7 +112,7 @@ const AdviserDashboard = () => {
 
       {/* Dashboard Buttons */}
       <div className="grid grid-cols-2 gap-6 px-4">
-        <DashboardButton iconSrc="/view-students-icon.svg" text="View Students" color="#0066CC" />
+        <DashboardButton iconSrc="/view-students-icon.svg" text="View Students" color="#0066CC" to={`/adviser/classes/${advisorySection.id}/students`} />
         <DashboardButton iconSrc="/attendance-icon.svg" text="Attendance" color="#28A428" />
         <DashboardButton iconSrc="/grades-icon.svg" text="Grades" color="#CA8E02" />
         <DashboardButton iconSrc="/reports-icon.svg" text="Reports" color="#8F28A4" />
