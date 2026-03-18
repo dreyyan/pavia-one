515f6fcb44832749d46c00b36259c463287982b2
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-18 18:32:47 +0800

    feat: initial setup for school registeres and forms page

---
+++ frontend/src/pages/adviser/AdviserSchoolRegistersForms.tsx
@@ -0,0 +1,12 @@
+const AdviserSchoolRegistersForms = () => {
+    return (
+        <div className="py-6 px-4 space-y-4">
+            <div className="bg-[var(--color-primary-700)] py-2 rounded-lg">
+                <h1 className="text-center text-[var(--color-text-50)]">School Registers & Forms</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default AdviserSchoolRegistersForms;
