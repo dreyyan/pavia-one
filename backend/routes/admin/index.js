2debc39b2064c8312e01bd63d0dc1da15d6aa85e
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-21 01:59:00 +0800

    chore(backend/admin): remove whitespace in index

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.use('/users', require('./users'));
 router.use('/students', require('./students'));
 router.use('/advisers', require('./advisers'));
