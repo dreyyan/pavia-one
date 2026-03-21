fd8764bc400d8ccf8d1bc4a3a4a4e1a1d5831ebc
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-21 23:54:51 +0800

    feat: CRUD operations for admin profile

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/profile', require('./profile'));
 router.use('/users', require('./users'));
 router.use('/students', require('./students'));
 router.use('/advisers', require('./advisers'));
