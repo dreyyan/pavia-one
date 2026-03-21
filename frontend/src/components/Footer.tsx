5c92335cf65a9b7b8e6031fb764f6f81c74e707b
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-21 12:27:07 +0800

    style: improve layout and avoid clutter in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,25 +1,14 @@
 const Footer = () => {
     return (
         <footer className="flex flex-col py-4 px-3 justify-center items-center gap-y-1 bg-[var(--color-primary-700)]">
-            {/* <img src="/pavia-one-banner-white.svg" className="" /> */}
-            <p className="font-roboto text-xs text-white">© 2026 PaviaOne. All rights reserved.</p>
+            <div className="pb-2">
+                <img src="/pavia-one-footer-banner.svg" className="" />
+            </div>
+            <p className="font-roboto text-xs text-[var(--color-text-100)]">© 2026 PaviaOne. All rights reserved.</p>
             <span className="flex gap-x-1">
-                <p className="font-roboto text-xs text-white">Need help? Contact</p>
-                <a href="" className="font-roboto text-xs text-white underline">support@paviaone.ph</a>
+                <p className="font-roboto text-xs text-[var(--color-text-100)]">Need help? Contact</p>
+                <a href="" className="font-roboto text-xs text-[var(--color-text-50)] underline">pnhspaviaone@gmail.com</a>
             </span>
-
-            {/* Social Links */}
-            {/* <div className="flex [&>span]:flex [&>span]:items-center [&>span]:gap-x-2 [&>span>img]:size-6 [&>span>a]:text-xs [&>span>a]:underline">
-                <span>
-                    <img src="/facebook-icon.svg" />
-                    <a href="" className="font-roboto text-xs text-white underline">facebook.com/pavianhs</a>
-                </span>
-
-                <span>
-                    <img src="/email-icon.svg" />
-                    <a href="" className="font-roboto text-xs text-white underline">facebook.com/pavianhs</a>
-                </span>
-            </div> */}
         </footer>
     );
 };
