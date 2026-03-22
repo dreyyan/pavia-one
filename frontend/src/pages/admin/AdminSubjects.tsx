0aec92cc4b007da6fe7fb6799224e83bebec573a
Sean <seanbenedict.besana@wvsu.edu.ph>
2026-03-23 00:41:27 +0800

    Revert "fix(admin/admin-subjects): fixed bulk delete and update curriculum not working (part 2)"
    This reverts commit 4e532cfe894f40c5174ae1cb113611168807b7bd.

--- frontend/src/pages/admin/AdminSubjects.tsx
+++ frontend/src/pages/admin/AdminSubjects.tsx
@@ -420,7 +420,7 @@ const AdminSubjects = () => {
             ? handleSubmit 
             : modalTitle.includes("Delete") && !modalTitle.includes("Failed") 
             ? onConfirmAction 
-            : async () => setShowModal(false)
+            : () => setShowModal(false)
         }
         loading={loading}
         formData={formData}
