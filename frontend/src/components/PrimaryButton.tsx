5c5d26d5aad4cfcfbf2f391071ee34bf3b211a3f
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-30 00:41:22 +0800

    refactor(primary-button): change hover style behavior to darken

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -4,20 +4,35 @@ interface PrimaryButtonProps {
     disabled?: boolean;
     color?: string;
     iconSrc?: string;
+    iconPosition?: "left" | "right";
 }
 
-const PrimaryButton: React.FC<PrimaryButtonProps> = ({ text, onClick, disabled, color, iconSrc }) => {
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+    text,
+    onClick,
+    disabled,
+    color,
+    iconSrc,
+    iconPosition = "left",
+}) => {
     return (
         <button
             onClick={onClick}
             disabled={disabled}
             style={color ? { backgroundColor: `#${color}` } : undefined}
-            className="flex justify-center gap-x-1 w-full py-3 rounded-md cursor-pointer bg-[var(--color-primary-600)] transition-all duration-200 hover:opacity-80 disabled:opacity-50"
+            className="flex justify-center items-center gap-1 w-full py-3 rounded-md cursor-pointer bg-[var(--color-primary-600)] transition-all duration-200 hover:bg-[var(--color-primary-700)] disabled:opacity-50"
         >
-            {iconSrc && (
-                <img src={iconSrc} />
+            {/* Render icon on the left */}
+            {iconSrc && iconPosition === "left" && (
+                <img src={iconSrc} className="size-4" />
             )}
+
             <p className="button text-white">{text}</p>
+
+            {/* Render icon on the right */}
+            {iconSrc && iconPosition === "right" && (
+                <img src={iconSrc} className="size-4" />
+            )}
         </button>
     );
 };
