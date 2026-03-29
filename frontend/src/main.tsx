d09400c665a6a42b00dab91d0899afad23819005
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-29 19:58:06 +0800

    fix(main): remove extra '.tsx' from import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from "./context/AuthProvider.tsx";
+import { AuthProvider } from "./context/AuthProvider";
 
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
