efec5dbcbedf6fe49b508c4d3f71a18ea849a934
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-31 16:20:24 +0800

    feat(admin/students): add empty state for students

--- frontend/src/pages/admin/AdminStudents.tsx
+++ frontend/src/pages/admin/AdminStudents.tsx
@@ -8,6 +8,7 @@ import Skeleton from "../../components/Skeleton";
 import PrimaryButton from "../../components/PrimaryButton";
 import CrudModal from "../../components/CrudModal";
 import StudentFormModal from "../../components/forms/StudentFormModal";
+import EmptyState from "../../components/EmptyState";
 
 // ? [CONSTANTS]
 const sexOptions = [
@@ -489,6 +490,22 @@ const AdminStudents = () => {
 
       {/* [CARDS] Students - Mobile View */}
       <div className="flex flex-col gap-4 sm:hidden mt-2 bg-[var(--color-bg-100)] px-3 py-4 rounded-lg">
+          {/* [EMPTY STATE] No Students */}
+          {!loading && students.length === 0 && (
+            <EmptyState
+              title="No students found"
+              subtitle="You currently have no assigned students. Please contact admin if this is an error."
+              iconSrc="/no-data-icon.svg"
+            />
+          )}
+          {/* [EMPTY STATE] No Students /w Current Filters */}
+          {!loading && displayedStudents.length === 0 && (
+            <EmptyState
+              title="No students found"
+              subtitle="No students match your current filters or search. Try adjusting your criteria."
+              iconSrc="/no-data-icon.svg"
+            />
+          )}
         {displayedStudents.map((s) => (
           <div
             key={s.id}
