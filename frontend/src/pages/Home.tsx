669aa4ee5a452c81d718f97e633e269f7ff9a2d4
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-03 08:41:13 +0800

    style(hero-section): extend to minimum height screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,15 +3,15 @@ import ImageHeader from "../components/ImageHeader";
 
 export default function Home() {
   return (
-    <main className="w-full">
-      {/* [COMPONENT] Image Header */}
-      <div>
+    <main className="w-full min-h-screen flex flex-col">
+      {/* Image Header */}
+      <div className="flex-shrink-0">
         <ImageHeader />
       </div>
 
-      {/* [SECTION] Features Carousel */}
-      <section className="px-4 sm:px-6 lg:px-8 py-10 sm:py-12 md:py-16 bg-[var(--color-bg-50)]">
-        <div className="w-full max-w-[1200px] mx-auto">
+      {/* Features Carousel */}
+      <section className="flex-1 px-4 sm:px-6 lg:px-8 py-10 sm:py-12 md:py-16 bg-[var(--color-bg-50)]">
+        <div className="w-full max-w-[1200px] mx-auto h-full">
           <FeaturesCarousel />
         </div>
       </section>
