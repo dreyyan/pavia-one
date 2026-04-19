045a2491943c9d5a81d79901f8c60149f8a86659
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-19 20:36:44 +0800

    style(delete-button): make responsiveness similar to other component buttons

--- frontend/src/components/buttons/DeleteButton.tsx
+++ frontend/src/components/buttons/DeleteButton.tsx
@@ -2,21 +2,29 @@ interface DeleteButtonProps {
   text?: string;
   onClick?: () => void;
   disabled?: boolean;
+  className?: string;
 }
 
 const DeleteButton: React.FC<DeleteButtonProps> = ({
   text = "Delete",
   onClick,
   disabled,
+  className = "",
 }) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className="flex justify-center items-center gap-1 w-full py-3 rounded-md cursor-pointer bg-[var(--color-red-600)] hover:bg-[var(--color-red-700)] transition-all duration-200 disabled:opacity-50"
+      className={`flex justify-center items-center gap-1 px-4 md:px-8 h-10 rounded-md cursor-pointer 
+        bg-[var(--color-red-600)] hover:bg-[var(--color-red-700)] 
+        transition-all duration-200 disabled:opacity-50 whitespace-nowrap ${className}`}
     >
-      <img src="/delete-icon-white.svg" className="size-4" />
-      <p className="button text-white">{text}</p>
+      <img 
+        src="/delete-icon-white.svg" 
+        className="size-4 sm:size-5" 
+        alt="Delete icon" 
+      />
+      <span className="button text-[var(--color-text-50)]">{text}</span>
     </button>
   );
 };
