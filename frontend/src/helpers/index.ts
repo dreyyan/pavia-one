d143ab1c2adcc8871c2fa857fa183bf89148ced1
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-19 14:35:59 +0800

    refactor(admin/students): move 'getVisiblePages' helper to a separate file

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -85,3 +85,26 @@ export const getGradeColor = (grade: number) => {
       };
   }
 };
+
+// [HELPER] Get visible pagination pages
+export const getVisiblePages = (current: number, total: number) => {
+  const delta = 1; // how many pages around current
+
+  const range: (number | "...")[] = [];
+  const left = Math.max(2, current - delta);
+  const right = Math.min(total - 1, current + delta);
+
+  range.push(1);
+
+  if (left > 2) range.push("...");
+
+  for (let i = left; i <= right; i++) {
+    range.push(i);
+  }
+
+  if (right < total - 1) range.push("...");
+
+  if (total > 1) range.push(total);
+
+  return range;
+};
